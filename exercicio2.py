7d1a6c25e5f2cec8b5b7d21c1ed6e2fe4974c2ce
leonardo111003 <leocatra123@hotmail.com>
2020-12-09 21:56:55 -0300

    feat: adiciona exercicio2

---
+++ exercicio2.py
@@ -0,0 +1,12 @@
+saque = int(input("digite o valor que sera retirado"))
+if saque > 0:
+ nota50 = saque/50
+ nota20 = saque/20
+ nota10 = saque/10
+ nota1 = saque/1
+ print (f"a quantidade de celudas de 50 reais foram {nota50}")
+ print (f"a quantidade de celudas de 50 reais foram {nota20}")
+ print (f"a quantidade de celudas de 50 reais foram {nota10}")
+ print (f"a quantidade de celudas de 50 reais foram {nota1}")
+else :
+ print("o usuario deve digitar um valor positivo e inteiro")
