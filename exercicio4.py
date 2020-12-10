4287438c301f392ed975141651f65b70918b5982
leonardo111003 <leocatra123@hotmail.com>
2020-12-09 22:00:13 -0300

    feat:adiciona exercicio4

---
+++ exercicio4.py
@@ -0,0 +1,5 @@
+nome = (input("digite seu nome"))
+nota1 = int(input("digite sua primeira nota"))
+nota2 = int(input("digite sua segunda nota"))
+media = (nota1 + nota2 )/ 2
+print (f"a media do aluno {nome}foi {media} ")
